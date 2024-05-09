3197e27a32ec5ca494fb63940a552d3336d2c434
manurec0 <72600413+manurec0@users.noreply.github.com>
2024-05-09 09:45:46 +0200

    Added Paco

--- SI_template/Assets/Scripts/tracking/PlayerMovement.cs
+++ SI_template/Assets/Scripts/tracking/PlayerMovement.cs
@@ -2,9 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Vector3 DebugPos1;
+    public Vector3 DebugPos2;
+    public TextMeshProUGUI Paco1;
+    public TextMeshProUGUI Paco2;
+    public GameObject player1;
+    public GameObject player2;
     // Start is called before the first frame update
     public Quaternion q;
     public bool manual;
@@ -17,7 +24,10 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         if(transform.position.y < -1) SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    
+        DebugPos1 = new Vector3(player1.transform.position.x, player1.transform.position.y, player1.transform.position.z);
+        DebugPos2 = new Vector3(player2.transform.position.x, player2.transform.position.y, player2.transform.position.z);
+        Paco1.text = DebugPos1.ToString();
+        Paco2.text = DebugPos2.ToString();
     }
 
     public void setPosition(Vector3 pos)
