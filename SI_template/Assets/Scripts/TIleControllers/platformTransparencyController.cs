e6cea4aef8cd5f5d0712c9ad83ce4be59946efc0
Víctor <victor.puerta01@estudiant.upf.edu>
2024-06-03 10:18:55 +0200

    build cracked

--- SI_template/Assets/Scripts/TIleControllers/platformTransparencyController.cs
+++ SI_template/Assets/Scripts/TIleControllers/platformTransparencyController.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 
 public class platformTransparencyController : MonoBehaviour, IPlateAction
 {
-    public GameObject crackedObject; 
-    public GameObject hideObject;    
+    public GameObject crackedObject;
+    public GameObject hideObject;
     public float transitionTime = 2f;
     public AudioSource transitionSound;
 
@@ -13,31 +13,37 @@ public class platformTransparencyController : MonoBehaviour, IPlateAction
 
     void Start()
     {
-        SetOpacity(hideObject, 0);
+        //SetOpacity(hideObject, 0);
         hideObject.SetActive(false);
 
-        SetOpacity(crackedObject, 1);
+        //SetOpacity(crackedObject, 1);
     }
 
     public void ExecuteAction(bool isActive)
     {
+        /*
         if (currentTransition != null)
         {
             StopCoroutine(currentTransition);
         }
         currentTransition = FadeTransition(isActive);
         StartCoroutine(currentTransition);
+        */
+        hideObject.SetActive(isActive);
+        boxCollider = GetComponent<BoxCollider>();
+        boxCollider.enabled = !boxCollider.enabled;
+
     }
 
     public void SetOnPause(bool pause)
     {
-        
+
     }
 
     private IEnumerator FadeTransition(bool toActive)
     {
         float elapsed = 0;
-        
+
         hideObject.SetActive(toActive);
         transitionSound.Play();
         StartCoroutine(AudioManager.PitchUp(transitionSound, 1.7f, transitionTime));
